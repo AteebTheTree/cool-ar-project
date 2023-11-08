dd47a23a60d296f0f17224124791c2f857aade1b
AteebTheTree <70818418+AteebTheTree@users.noreply.github.com>
2023-11-08 18:47:38 -0500

    Add files via upload

--- markerhandler.js
+++ markerhandler.js
@@ -1,14 +1,33 @@
+var playersArray = [];
 AFRAME.registerComponent("markerhandler", {
     init: async function() {
-        var players = await this.getPlayers();
-
+        console.log("this is a marker handler, and I am doing my job")
         this.el.addEventListener("markerFound", () => {
             var playerName = this.el.getAttribute("player_name");
-            var barcodeValue = this.el.getAttribute("barcode_vale")
+            var barcodeValue = this.el.getAttribute("value")
+            playersArray.push({player_name: playerName, barcode_value: barcodeValue})
+
+            var player = document.querySelector(`#${playerName}-${barcodeValue}`)
+            player.setAttribute("visible", true)
+        })
+
+        this.el.addEventListener("markerLost", () => {
+            var playerName = this.el.getAttribute("player_name")
+            var index = playersArray.findIndex(x => x.player_name = playerName);
+            if(index > -1) {
+                playersArray.splice(index, 1)
+            }
         })
     },
+    tick: function() {
+        if(playersArray.length > 1)
+        {
+            var player = this.getPlayers();
+        }
+    },
     getPlayers: function() {
-        return fetch("players.json")
+        console.log("Wow, I just got a player!")
+        return fetch("myplayers.json")
         .then(res => res.json())
         .then(data => data)
     },
--- players.js
+++ players.js
@@ -1,20 +1,23 @@
 AFRAME.registerComponent("players", {
     init: async function() {
+        console.log("Im the player script, and im working!")
         var players = await this.getPlayers()
 
         var barcodes = Object.keys(players)
 
         barcodes.map(e => {
-            var element = players[e];
-            this.createPlayers(element)
+            var player = players[e];
+            this.createPlayers(player)
         })
     },
     getPlayers: function() {
-        return fetch("players.json")
+        console.log("Wow, I just got a player!")
+        return fetch("myplayers.json")
         .then(res => res.json())
         .then(data => data)
     },
     createPlayers: async function(json_data) {
+        console.log("If this code is somehow working, I have no clue what's going on!")
         var playerName = json_data.player_name;
         var barcodeValue = json_data.barcodeValue;
         var countingStats = json_data.countingStats;
@@ -26,7 +29,7 @@ AFRAME.registerComponent("players", {
 
         marker.setAttribute("id", `marker-${barcodeValue}`);
         marker.setAttribute("type", "barcode");
-        marker.setAttribute("element_name", playerName);
+        marker.setAttribute("player_name", playerName);
         marker.setAttribute("value", barcodeValue);
         marker.setAttribute("markerhandler", {});
 
@@ -35,5 +38,15 @@ AFRAME.registerComponent("players", {
         var player = document.createElement('a-entity');
         player.setAttribute('id', `${playerName}-${barcodeValue}`)
         marker.appendChild(player)
+
+        var card = document.createElement('a-entity');
+        card.setAttribute("geometry", {
+            primitive: "plane",
+            width: 1,
+            height: 1
+        });
+        card.setAttribute("position", { x: 0, y: 0, z: 0 });
+        card.setAttribute("rotation", { x: -90, y: 0, z: 0 });
+        player.appendChild(card)
     }
 })
